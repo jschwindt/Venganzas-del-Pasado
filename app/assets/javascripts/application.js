887b9fdea7a50c06e56f44b0477084583f80f497
Juan Schwindt <juan@schwindt.org>
2011-11-07 19:23:03 -0300

    JPlayer with simplify design & multiple players per page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,26 @@
 //= require bootstrap-alerts
 //= require jquery.jplayer
 //= require_self
+
+$(document).ready(function() {
+
+  $.jPlayer.timeFormat.showHour = true;
+
+  $(".jp-audio").each(function() {
+    var container_id = $(this).attr('id');
+    var url = $(this).attr('data-url');
+    var jplayer_id = $(this).find('.jp-jplayer').attr('id');
+    $('#' + jplayer_id).jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", { mp3: url });
+      },
+      play: function() { // To avoid both jPlayers playing together.
+        $(this).jPlayer("pauseOthers");
+      },
+      cssSelectorAncestor: '#' + container_id,
+      swfPath: "/assets",
+      supplied: "mp3"
+    });
+  });
+});
+
