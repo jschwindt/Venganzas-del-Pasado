431d93f383084054dfa7251e66fb198ed9619563
Juan Schwindt <juan@schwindt.org>
2011-11-13 17:27:19 -0300

    Add inherited_resources for posts_controller

--- Gemfile
+++ Gemfile
@@ -9,14 +9,14 @@ gem 'mysql2'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'coffee-rails', "~> 3.1.1"
+  gem 'coffee-rails', '~> 3.1.1'
   gem 'uglifier',     '>= 1.0.3'
 end
 
 gem 'jquery-rails'
 gem 'haml-rails'
 gem 'kaminari'
-gem "friendly_id", "~> 4.0.0.beta14"
+gem 'friendly_id', '~> 4.0.0.beta14'
 gem 'sequel'
 gem 'redcarpet', '~> 2.0.0b5'
 gem 'omniauth'
@@ -24,15 +24,17 @@ gem 'sass-rails',   '~> 3.1.4'
 gem 'meta_search',    '>= 1.1.0.pre'
 gem 'activeadmin'
 gem 'bootstrap-sass'
+gem 'inherited_resources'
 
 group :development do
   gem 'nifty-generators'
   gem 'hirb'
   gem 'wirble'
+  gem 'rails_best_practices'
 end
 
 group :test do
   # Pretty printed test output
   # gem 'turn', :require => false
-  gem "mocha"
+  gem 'mocha'
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,10 @@
-class PostsController < ApplicationController
-  def index
-    @posts = Post.published.lifo.page params[:page]
+# encoding: utf-8
+
+class PostsController < InheritedResources::Base
+
+  protected
+  def collection
+    @posts ||= end_of_association_chain.published.lifo.page(params[:page])
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(params[:post])
-    if @post.save
-      redirect_to @post, :notice => "Successfully created post."
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    if @post.update_attributes(params[:post])
-      redirect_to @post, :notice  => "Successfully updated post."
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_url, :notice => "Successfully destroyed post."
-  end
 end
