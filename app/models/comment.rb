a9a61158e5676d67b9c585fe9c439d5bf547d516
Juan Schwindt <juan@schwindt.org>
2011-11-14 12:55:37 -0300

    Add users/comments relation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :post, :counter_cache => true
-  
+  belongs_to :user
+
   validates :content, :presence => true
-  
+
   scope :approved, where(:status => 'approved')
   scope :fifo, order('created_at ASC')
-  
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :comments, :dependent => :nullify
+
+  validates :alias, :presence => true, :uniqueness => { :case_sensitive => false }
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +10,5 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
 end
