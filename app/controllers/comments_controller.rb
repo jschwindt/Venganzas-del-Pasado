16aa132bf33576526043734b66014fb3439dc562
Juan Schwindt <juan@schwindt.org>
2011-11-13 21:48:33 -0300

    Add inherited_resources to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,49 +1,22 @@
-class CommentsController < ApplicationController
-  before_filter :find_post
-  
+# encoding: utf-8
+
+class CommentsController < InheritedResources::Base
+  belongs_to :post
+
   def index
+    @post = Post.find params[:post_id]
     page = params[:page].present? ? params[:page] : @post.comments.approved.page.num_pages
     @comments = @post.comments.approved.fifo.page page
   end
 
-  def show
-    @comment = @post.comments.find(params[:id])
-  end
-
-  def new
-    @comment = @post.comments.new
-  end
-
   def create
-    @comment = @post.comments.new(params[:comment])
-    if @comment.save
-      redirect_to post_comments_path(@post), :notice => "Successfully created comment."
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @comment = @post.comments.find(params[:id])
+    create! { collection_url }
   end
 
   def update
-    @comment = @post.comments.find(params[:id])
-    if @comment.update_attributes(params[:comment])
-      redirect_to post_comments_path(@post), :notice  => "Successfully updated comment."
-    else
-      render :action => 'edit'
+    update! do |success, failure|
+      success.html { redirect_to collection_url }
     end
   end
-
-  def destroy
-    @comment = @post.comments.find(params[:id])
-    @comment.destroy
-    redirect_to post_comments_path(@post), :notice => "Successfully destroyed comment."
-  end
   
-private
-  def find_post
-    @post = Post.find params[:post_id]
-  end
 end
