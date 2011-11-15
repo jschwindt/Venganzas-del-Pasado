3286e85207843896034f71b7faa6bd72f7bf0e1c
Juan Schwindt <juan@schwindt.org>
2011-11-14 23:30:11 -0300

    Make user confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :alias, :use => :slugged
+
   has_many :comments, :dependent => :nullify
 
   validates :alias, :presence => true, :uniqueness => { :case_sensitive => false }
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :alias
 
 end
