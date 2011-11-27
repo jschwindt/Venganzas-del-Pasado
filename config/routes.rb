a93fae2f9648d27d16beeecf840073d4e7444260
Juan Schwindt <juan@schwindt.org>
2011-11-27 15:45:07 -0300

    Add FB Login button and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ VenganzasDelPasado::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
-    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
+    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru', :as => :user_omniauth
   end
 
   resources :posts do
