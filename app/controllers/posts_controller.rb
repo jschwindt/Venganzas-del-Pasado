e8cce5575cae0894b11dd1e2a1a3792d5fccfaf1
Federico Cingolani <mail@fcingolani.com.ar>
2011-12-04 18:11:36 -0300

    Removido inherited_resources en posts_controller.rb, y se aplicaron los rails_best_practices sugeridos.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,33 @@
-# encoding: utf-8
-
-class PostsController < InheritedResources::Base
+class PostsController < ApplicationController
   load_and_authorize_resource
-  
+  before_filter :find_post, :only => [:show, :edit, :update]
+
+  def index
+    @posts = Post.published.lifo.page(params[:page]).per(10)
+  end
+
   def show
-    show! do | format |
-      comments_collection = @post.comments.approved_or_from_user(current_user).fifo
-      
-      page = params[:page].present? ? params[:page] : comments_collection.page.num_pages
-      @comments = comments_collection.page page
+    comments_collection = @post.comments.approved_or_from_user(current_user).fifo
+    page = params[:page].present? ? params[:page] : comments_collection.page.num_pages
+    @comments = comments_collection.page page
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(params[:post])
+      flash[:notice] = "Successfully updated post."
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
-  
+
   protected
-  def collection
-    @posts ||= end_of_association_chain.published.lifo.page(params[:page]).per(5)
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
