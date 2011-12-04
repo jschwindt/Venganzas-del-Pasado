ccc73348753240278006a66623fad11c6a3f1d36
Federico Cingolani <mail@fcingolani.com.ar>
2011-12-03 21:59:19 -0300

    Paginación de Comentarios

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,13 @@
 
 class PostsController < InheritedResources::Base
   
+  def show
+    show! do | format |
+      page = params[:page].present? ? params[:page] : @post.comments.approved.page.num_pages
+      @comments = @post.comments.approved.fifo.page page
+    end
+  end
+  
   protected
   def collection
     @posts ||= end_of_association_chain.published.lifo.page(params[:page]).per(5)
