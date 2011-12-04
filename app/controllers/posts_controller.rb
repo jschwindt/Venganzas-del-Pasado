2b6b2253a32ea5402b398b7c2918ee5c0e88eb83
Federico Cingolani <mail@fcingolani.com.ar>
2011-12-04 15:44:28 -0300

    Simplificando app/controllers/posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,8 @@ class PostsController < InheritedResources::Base
   
   def show
     show! do | format |
-      if(current_user)
-        comments_collection = @post.comments.approved_or_from_user(current_user).fifo
-      else
-        comments_collection = @post.comments.approved.fifo
-      end
-    
+      comments_collection = @post.comments.approved_or_from_user(current_user).fifo
+      
       page = params[:page].present? ? params[:page] : comments_collection.page.num_pages
       @comments = comments_collection.page page
     end
