881aaf0612df78a54efb29c9316e45d08558e007
Federico Cingolani <mail@fcingolani.com.ar>
2011-12-05 21:37:00 -0300

    Abriendo la puerta para el borrado lógico. approved_or_from_user ahora verifica que los comentarios listados no tengan status = 'deleted'.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,7 +11,7 @@ class Comment < ActiveRecord::Base
 
   def self.approved_or_from_user( user )
     if user
-      where( 'status = ? OR user_id = ?', 'approved', user.id )
+      where( 'status = ? OR (user_id = ? AND status != ?)', 'approved', user.id, 'deleted' )
     else
       approved
     end
@@ -30,6 +30,8 @@ class Comment < ActiveRecord::Base
     end
 
     self.gravatar_hash = user.gravatar_hash
+
+    self
   end
 
 end
