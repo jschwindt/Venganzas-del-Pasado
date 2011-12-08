403ffebba1b947a2271783893e1f930c81912e38
Federico Cingolani <mail@fcingolani.com.ar>
2011-12-08 13:30:56 -0300

    Se reemplazaron los Activities de la home por Comentarios Recientes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @posts = Post.published.lifo.limit(VenganzasDelPasado::Application.config.home_posts_count)
+    @comments = Comment.approved_or_from_user(current_user).lifo.limit(VenganzasDelPasado::Application.config.home_comments_count)
   end
 
 end
--- config/initializers/venganzasdelpasado.rb
+++ config/initializers/venganzasdelpasado.rb
@@ -4,6 +4,7 @@ module VenganzasDelPasado
 
     config.good_user_karma_treshold = 500
     config.home_posts_count = 3
+    config.home_comments_count = 5
     config.posts_per_page = 5
     config.comments_per_page = 20
 
