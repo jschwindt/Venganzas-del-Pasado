a728ce5fdc3c78b65f51be906b27e2f3a9f94119
Juan Schwindt <juan@schwindt.org>
2011-12-08 15:42:07 -0300

    Improve abilities and its tests

--- test/unit/ability_test.rb
+++ test/unit/ability_test.rb
@@ -2,42 +2,67 @@ require 'test_helper'
 
 class AbilityTest < ActiveSupport::TestCase
 
-  test "user can only edit comments which he owns" do
-    user = users(:one)
-    ability = Ability.new(user)
-    assert ability.can?(:edit, Comment.new(:user => user))
-    assert ability.cannot?(:edit, Comment.new)
+  #---------------------------------------------
+  # Tests for anonymous users
+  #---------------------------------------------
+
+  test "logout user can read only published post" do
+    ability = Ability.new(nil)
+    assert ability.can?(:read, posts(:published))
+    assert ability.cannot?(:read, posts(:draft))
   end
 
-  test "user can only destroy comments which he owns" do
+  test "logout user can read only approved comments" do
+    ability = Ability.new(nil)
+    assert ability.can?(:read, comments(:approved))
+    assert ability.cannot?(:read, comments(:pending))
+  end
+
+  test "logout user can read articles" do
+    ability = Ability.new(nil)
+    assert ability.can?(:read, articles(:one))
+  end
+
+  test "logout user can read other users profile" do
+    ability = Ability.new(nil)
+    assert ability.can?(:read, users(:one))
+  end
+
+  #---------------------------------------------
+  # Tests for logged in plain user
+  #---------------------------------------------
+
+  test "user can create comments" do
     user = users(:one)
     ability = Ability.new(user)
-    assert ability.can?(:destroy, Comment.new(:user => user))
+    assert ability.can?(:create, user.comments.new)
+  end
+
+  test "user can read his/her pending comments but no other's" do
+    user = users(:one)
+    ability = Ability.new(user)
+    assert ability.can?(:read, user.comments.new)
+    assert ability.cannot?(:read, comments(:pending))
+  end
+
+  test "user cannot update any comments" do
+    user = users(:one)
+    ability = Ability.new(user)
+    assert ability.cannot?(:update, user.comments.new)
+    assert ability.cannot?(:update, Comment.new)
+  end
+
+  test "user cannot destroy any comments" do
+    user = users(:one)
+    ability = Ability.new(user)
+    assert ability.cannot?(:destroy, user.comments.new)
     assert ability.cannot?(:destroy, Comment.new)
   end
 
-  test "moderators can update comments" do
-    user = users(:moderator)
+  test "user can flag any comments" do
+    user = users(:one)
     ability = Ability.new(user)
-    assert ability.can?(:update, Comment.new)
-  end
-
-  test "moderators can destroy comments" do
-    user = users(:moderator)
-    ability = Ability.new(user)
-    assert ability.can?(:destroy, Comment.new)
-  end
-
-  test "editors can update comments" do
-    user = users(:editor)
-    ability = Ability.new(user)
-    assert ability.can?(:update, Comment.new)
-  end
-
-  test "editors can destroy comments" do
-    user = users(:editor)
-    ability = Ability.new(user)
-    assert ability.can?(:destroy, Comment.new)
+    assert ability.can?(:flag, Comment.new)
   end
 
   test "good users can approve comments" do
@@ -45,6 +70,96 @@ class AbilityTest < ActiveSupport::TestCase
     user.karma = 600
     ability = Ability.new(user)
     assert ability.can?(:approve, Comment.new)
+  end
+
+  test "user cannot manage all" do
+    user = users(:one)
+    ability = Ability.new(user)
+    assert ability.cannot?(:manage, :all)
+  end
+
+  #---------------------------------------------
+  # Tests for moderators
+  #---------------------------------------------
+
+  test "moderators can approve comments" do
+    user = users(:moderator)
+    ability = Ability.new(user)
+    assert ability.can?(:approve, Comment.new)
+  end
+
+  test "moderators can trash comments" do
+    user = users(:moderator)
+    ability = Ability.new(user)
+    assert ability.can?(:trash, Comment.new)
+  end
+
+  test "moderators cannot update comments" do
+    user = users(:moderator)
+    ability = Ability.new(user)
+    assert ability.cannot?(:update, Comment.new)
+  end
+
+  test "moderators cannot destroy comments" do
+    user = users(:moderator)
+    ability = Ability.new(user)
+    assert ability.cannot?(:destroy, Comment.new)
+  end
+
+  test "moderators cannot manage all" do
+    user = users(:moderator)
+    ability = Ability.new(user)
+    assert ability.cannot?(:manage, :all)
+  end
+
+  #---------------------------------------------
+  # Tests for editors
+  #---------------------------------------------
+
+  test "editors can approve comments" do
+    user = users(:editor)
+    ability = Ability.new(user)
+    assert ability.can?(:approve, Comment.new)
+  end
+
+  test "editors can trash comments" do
+    user = users(:editor)
+    ability = Ability.new(user)
+    assert ability.can?(:trash, Comment.new)
+  end
+
+  test "editors cannot update comments" do
+    user = users(:editor)
+    ability = Ability.new(user)
+    assert ability.cannot?(:update, Comment.new)
+  end
+
+  test "editors cannot destroy comments" do
+    user = users(:editor)
+    ability = Ability.new(user)
+    assert ability.cannot?(:destroy, Comment.new)
+  end
+
+  test "editors can update posts" do
+    user = users(:editor)
+    ability = Ability.new(user)
+    assert ability.can?(:update, posts(:one))
+  end
+
+  test "editors cannot manage all" do
+    user = users(:editor)
+    ability = Ability.new(user)
+    assert ability.cannot?(:manage, :all)
+  end
+
+  #---------------------------------------------
+  # Tests for admins
+  #---------------------------------------------
+
+  test "admins can do anything" do
+    user = users(:admin)
+    ability = Ability.new(user)
+    assert ability.can?(:manage, :all)
   end
 
 end
