1fa0467ab1efca808a8f6ea3bbaa7a1c3966080d
Juan Schwindt <juan@schwindt.org>
2011-12-15 00:41:09 -0300

    WIP on functional tests

--- test/functional/admin/comments_controller_test.rb
+++ test/functional/admin/comments_controller_test.rb
@@ -1,7 +1,76 @@
 require 'test_helper'
 
 class Admin::CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+      sign_in users(:admin)
+    end
+
+    def test_index
+      get :index, :post_id => posts(:one)
+      assert_not_nil assigns(:post)
+      assert_not_nil assigns(:comments)
+      assert_response :success
+      assert_template 'index'
+    end
+
+    def test_show
+      get :show, :post_id => posts(:one), :id => comments(:one)
+      assert_not_nil assigns(:post)
+      assert_not_nil assigns(:comment)
+      assert_response :success
+      assert_template 'show'
+    end
+
+    def test_new
+      get :new, :post_id => posts(:one)
+      assert_not_nil assigns(:post)
+      assert_not_nil assigns(:comment)
+      assert_response :success
+      assert_template 'new'
+    end
+
+    def test_create_invalid
+  #    Comment.any_instance.stubs(:valid?).returns(false)
+      post :create, :post_id => posts(:one), :comment => {}
+      assert_not_nil assigns(:post)
+      assert_not_nil assigns(:comment)
+      assert_response :success
+      assert_template 'new'
+    end
+
+    def test_create_valid
+      Comment.any_instance.stubs(:valid?).returns(true)
+      post :create, :post_id => posts(:one), :comment => { :content => 'testing' }
+      assert_redirected_to post_comments_url(assigns(:post))
+    end
+
+    def test_edit
+      get :edit, :post_id => posts(:one), :id => comments(:one)
+      assert_not_nil assigns(:post)
+      assert_not_nil assigns(:comment)
+      assert_response :success
+      assert_template 'edit'
+    end
+
+    def test_update_invalid
+      Comment.any_instance.stubs(:valid?).returns(false)
+      put :update, :post_id => posts(:one), :id => comments(:one)
+      assert_not_nil assigns(:post)
+      assert_not_nil assigns(:comment)
+      assert_response :success
+      assert_template 'edit'
+    end
+
+    def test_update_valid
+      Comment.any_instance.stubs(:valid?).returns(true)
+      put :update, :post_id => posts(:one), :id => comments(:one)
+      assert_redirected_to post_comments_url(assigns(:post))
+    end
+
+    def test_destroy
+      comment = Comment.first
+      delete :destroy, :post_id => posts(:one), :id => comment
+      assert_redirected_to post_comments_url(assigns(:post))
+      assert !Comment.exists?(comment.id)
+    end
 end
--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -19,24 +19,4 @@ class PostsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:post)
   end
 
-  def test_edit
-    get :edit, :id => posts(:one)
-    assert_response :success
-    assert_template 'edit'
-    assert_not_nil assigns(:post)
-  end
-
-  def test_update_invalid
-    put :update, :id => posts(:one), :post => {:title => ''}
-    assert_not_nil assigns(:post)
-    assert_response :success
-    assert_template 'edit'
-  end
-
-  def test_update_valid
-    Post.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => posts(:one)
-    assert_redirected_to post_url(assigns(:post))
-  end
-
 end
