e67cd01ac7a1eff9467d4c1892460ead78fbbfe5
Juan Schwindt <juan@schwindt.org>
2011-12-17 18:54:59 -0300

    Fix comments pagination bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,9 @@ class PostsController < ApplicationController
 
   def show
     comments_collection = @post.comments.approved_or_from_user(current_user).fifo
-    page = params[:page].present? ? params[:page] : comments_collection.page.num_pages
+    page = params[:page].present? ?
+             params[:page] :
+             comments_collection.page.per(VenganzasDelPasado::Application.config.comments_per_page).num_pages
     @comments = comments_collection.page(page).per(VenganzasDelPasado::Application.config.comments_per_page)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
 
   def show
     comments_collection = @user.comments.approved_or_from_user(current_user).fifo
-    page = params[:page].present? ? params[:page] : comments_collection.page.num_pages
+    page = params[:page].present? ?
+             params[:page] :
+             comments_collection.page.per(VenganzasDelPasado::Application.config.comments_per_page).num_pages
     @comments = comments_collection.page(page).per(VenganzasDelPasado::Application.config.comments_per_page)
   end
 
