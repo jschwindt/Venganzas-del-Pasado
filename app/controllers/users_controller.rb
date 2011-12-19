dcc3cfbcf2be6c722c1c06964651619d9743e575
Federico Cingolani <mail@fcingolani.com.ar>
2011-12-19 17:30:40 -0300

    El perfil de usuarios ahora lista los comentarios de mas recientes a mas viejos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,8 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def show
-    comments_collection = @user.comments.approved_or_from_user(current_user).fifo
-    page = params[:page].present? ?
-             params[:page] :
-             comments_collection.page.per(VenganzasDelPasado::Application.config.comments_per_page).num_pages
-    @comments = comments_collection.page(page).per(VenganzasDelPasado::Application.config.comments_per_page)
+    comments_collection = @user.comments.approved_or_from_user(current_user).lifo
+    @comments = comments_collection.page(params[:page]).per(VenganzasDelPasado::Application.config.comments_per_page)
   end
 
 end
