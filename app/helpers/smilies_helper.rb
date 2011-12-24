778fa9d94f86ecb8d44c378b93b77f030c331fe9
Federico Cingolani <mail@fcingolani.com.ar>
2011-12-24 01:21:24 -0300

    Emoticonos! _Inspirado_ en el parser de smilies de WP, pero se muestran por CSS en lugar de usando img

---
+++ app/helpers/smilies_helper.rb
@@ -0,0 +1,103 @@
+module SmiliesHelper
+
+  TRANSLATIONS = {
+    ':mrgreen:' => 'mrgreen',
+    ':neutral:' => 'neutral',
+    ':twisted:' => 'twisted',
+      ':arrow:' => 'arrow',
+      ':shock:' => 'eek',
+      ':smile:' => 'smile',
+        ':???:' => 'confused',
+       ':cool:' => 'cool',
+       ':evil:' => 'evil',
+       ':grin:' => 'biggrin',
+       ':idea:' => 'idea',
+       ':oops:' => 'redface',
+       ':razz:' => 'razz',
+       ':roll:' => 'rolleyes',
+       ':wink:' => 'wink',
+        ':cry:' => 'cry',
+        ':eek:' => 'surprised',
+        ':lol:' => 'lol',
+        ':mad:' => 'mad',
+        ':sad:' => 'sad',
+          '8-)' => 'cool',
+          '8-O' => 'eek',
+          ':-(' => 'sad',
+          ':-)' => 'smile',
+          ':-?' => 'confused',
+          ':-D' => 'biggrin',
+          ':-P' => 'razz',
+          ':-o' => 'surprised',
+          ':-x' => 'mad',
+          ':-|' => 'neutral',
+          ';-)' => 'wink',
+           '8)' => 'cool',
+           '8O' => 'eek',
+           ':(' => 'sad',
+           ':)' => 'smile',
+           ':?' => 'confused',
+           ':D' => 'biggrin',
+           ':P' => 'razz',
+           ':o' => 'surprised',
+           ':x' => 'mad',
+           ':|' => 'neutral',
+           ';)' => 'wink',
+          ':!:' => 'exclaim',
+          ':?:' => 'question',
+  }
+
+  REGEXP = /(?m-ix:(?:\s|^);(?:\-\)|\))|(?:\s|^):(?:\||x|wink:|twisted:|smile:|shock:|sad:|roll:|razz:|oops:|o|neutral:|mrgreen:|mad:|lol:|idea:|grin:|evil:|eek:|cry:|cool:|arrow:|P|D|\?\?\?:|\?:|\?|\-\||\-x|\-o|\-P|\-D|\-\?|\-\)|\-\(|\)|\(|!:)|(?:\s|^)8(?:O|\-O|\-\)|\))(?:\s|$))/m
+
+  def replace_smileys(text)
+	  output = '';
+
+	  text.split(/(<.*?>)/, -1).each do |content|
+
+		  if ((content.length > 0) && ('<' != content[0]))
+
+		    content = content.gsub REGEXP do |smiley|
+		      smiley.strip!
+		      if smiley.nil?
+	          return ''
+          end
+          content_tag(:span, smiley, :class => "smiley "+TRANSLATIONS[smiley])
+		    end
+
+		  end
+		  output << content;
+
+	  end
+
+    output.html_safe
+  end
+
+  private
+
+  def generate_regexp
+	  regexp = '(?:\s|^)'
+
+	  subchar = ''
+
+	  TRANSLATIONS.sort.reverse_each do |smiley, img|
+	    firstchar = smiley[0]
+	    rest = smiley[1..smiley.length]
+
+	    if (firstchar != subchar)
+		    if (subchar != '')
+			    regexp << ')|(?:\s|^)'
+		    end
+		    subchar = firstchar
+		    regexp << Regexp.escape(firstchar) + '(?:'
+	    else
+		    regexp << '|'
+	    end
+	    regexp << Regexp.escape(rest)
+	  end
+
+	  regexp << ')(?:\s|$)'
+
+	  Regexp.new(regexp, Regexp::MULTILINE)
+  end
+
+end
