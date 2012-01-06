c745e77d578091370c2802f1aaaa27a80fcdc651
Juan Schwindt <juan@schwindt.org>
2012-01-06 00:01:04 -0300

    Escape search string

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,11 @@ class SearchController < ApplicationController
   def index
     @what = params[:what] == 'comments' ? 'comments' : 'posts'
     search_opts = { order: :created_at, sort_mode: :desc, match_mode: :boolean }
-    @facets = ThinkingSphinx.facets(params[:q], search_opts)
+    q = Riddle.escape(params[:q])   # see: https://github.com/freelancing-god/thinking-sphinx/issues/235
+    @facets = ThinkingSphinx.facets(q, search_opts)
     @facets[:class].reverse_merge!('Post' => 0, 'Comment' => 0 )
 
-    @results = @what.classify.constantize.search(params[:q], search_opts).
+    @results = @what.classify.constantize.search(q, search_opts).
                   page(params[:page]).
                   per(VenganzasDelPasado::Application.config.send "#{@what}_search_per_page")
   end
