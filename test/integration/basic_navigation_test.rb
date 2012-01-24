ed08f1a854f65a9d540e6ca42d2a794d8263196f
Juan Schwindt <juan@schwindt.org>
2012-01-24 19:01:16 -0300

    Improve navigation test

--- test/integration/basic_navigation_test.rb
+++ test/integration/basic_navigation_test.rb
@@ -6,12 +6,12 @@ class BasicNavigationTest < ActionDispatch::IntegrationTest
     post = Post.published.lifo.last
 
     visit "/"
-    assert page.has_css?('h3.title')
+    assert page.has_css?('h3.title', :count => VenganzasDelPasado::Application.config.home_posts_count)
     assert page.has_content?(post.title)
 
     click_on post.title
     assert_equal current_path, post_path(post)
-    assert page.has_css?('h3.title')
+    assert page.has_css?('h3.title', :count => 1)
     assert page.has_content?(post.title)
 
     click_on "Programas Recientes"
