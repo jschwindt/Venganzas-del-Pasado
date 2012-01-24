1585db8d98e8c10356244d90d64688164e95d209
Juan Schwindt <juan@schwindt.org>
2012-01-24 17:09:42 -0300

    Move constante into class

--- test/integration/redirects_test.rb
+++ test/integration/redirects_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
-REDIRECT_CASES = {
-  '/2011'                                             => '/posts/2011',
-  '/2011/12'                                          => '/posts/2011/12',
-  '/2011/12'                                          => '/posts/2011/12',
-  '/2011/12/09/'                                      => '/posts/2011/12/09',
-  '/2011/12/09/la-venganza-sera-terrible-2011-12-09/' => '/posts/la-venganza-sera-terrible-del-09-12-2011',
-  '/2011/12/page/5'                                   => '/posts/2011/12?page=5',
-  '/acerca-de'                                        => '/articles/acerca-de-este-sitio',
-  '/descargas'                                        => '/posts/descargas',
-  '/feed'                                             => '/posts.rss',
-  '/foro/'                                            => '/',
-  '/page/12'                                          => '/',
-  '/tags/pepe'                                        => '/',
-  '/torrent-feed'                                     => '/torrents.rss',
-}
-
 class RedirectsTest < ActionDispatch::IntegrationTest
+
+  REDIRECT_CASES = {
+    '/2011'                                             => '/posts/2011',
+    '/2011/12'                                          => '/posts/2011/12',
+    '/2011/12'                                          => '/posts/2011/12',
+    '/2011/12/09/'                                      => '/posts/2011/12/09',
+    '/2011/12/09/la-venganza-sera-terrible-2011-12-09/' => '/posts/la-venganza-sera-terrible-del-09-12-2011',
+    '/2011/12/page/5'                                   => '/posts/2011/12?page=5',
+    '/acerca-de'                                        => '/articles/acerca-de-este-sitio',
+    '/descargas'                                        => '/posts/descargas',
+    '/feed'                                             => '/posts.rss',
+    '/foro/'                                            => '/',
+    '/page/12'                                          => '/',
+    '/tags/pepe'                                        => '/',
+    '/torrent-feed'                                     => '/torrents.rss',
+  }
 
   test "redirects" do
     REDIRECT_CASES.each do |path, result|
