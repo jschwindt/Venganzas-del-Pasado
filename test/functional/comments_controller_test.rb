f27f6f717d7620b9a2c181c98d788c04c25e482f
Juan Schwindt <juan@schwindt.org>
2012-03-27 16:20:41 -0300

    Improve comments controller tests

--- test/functional/comments_controller_test.rb
+++ test/functional/comments_controller_test.rb
@@ -11,12 +11,22 @@ class CommentsControllerTest < ActionController::TestCase
     assert assigns(:comment)
     assert_redirected_to "#{post_path(assigns(:post))}#comment#{assigns(:comment).id}"
   end
-  
+
+  test "show comment" do
+    comment = comments(:approved)
+    get :show, :post_id => comment.post, :id => comment
+    assert assigns(:post)
+    assert assigns(:comment)
+    assert_template 'show'
+    assert_response :success
+  end
+
   test "flag comment" do
     comment = comments(:one)
     xhr :post, :flag, :id => comment
     assert assigns(:comment)
     assert_template 'flag'
+    assert_response :success
   end
 
   test "like comment" do
@@ -24,6 +34,7 @@ class CommentsControllerTest < ActionController::TestCase
     xhr :post, :like, :id => comment
     assert assigns(:comment)
     assert_template 'like'
+    assert_response :success
   end
 
   test "dislike comment" do
@@ -31,6 +42,7 @@ class CommentsControllerTest < ActionController::TestCase
     xhr :post, :dislike, :id => comment
     assert assigns(:comment)
     assert_template 'dislike'
+    assert_response :success
   end
 
 end
