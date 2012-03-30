3d4e3dd8a0bd18fcbab50304b909b46d6cfac54e
Juan Schwindt <juan@schwindt.org>
2012-03-30 17:36:17 -0300

    Move class methods into class << self block

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,50 +19,58 @@ class User < ActiveRecord::Base
 
   scope :lifo, order('created_at DESC')
 
-  def self.roles
-    ['', 'admin', 'editor', 'moderator']
-  end
+  class << self
+    def roles
+      ['', 'admin', 'editor', 'moderator']
+    end
 
-  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-    data = access_token.extra.raw_info
-    if user = User.where('email = ? OR fb_userid = ?', data.email, data.id).first
-      if user.email != data.email || user.fb_userid != data.id
-        user.email     = data.email
-        user.fb_userid = data.id
-        user.save!
+    def find_for_facebook_oauth(access_token, signed_in_resource=nil)
+      data = access_token.extra.raw_info
+      if user = User.where('email = ? OR fb_userid = ?', data.email, data.id).first
+        if user.email != data.email || user.fb_userid != data.id
+          user.email     = data.email
+          user.fb_userid = data.id
+          user.save!
+        end
+        user
+      else
+        create_from_facebook data
       end
+    end
+
+    def create_from_facebook(fb_data)
+      generated_password = Devise.friendly_token.first(10)
+      user = User.new( :email => fb_data.email,
+                  :alias      => fb_data.username || fb_data.name,
+                  :password   => generated_password,
+                  :password_confirmation => generated_password,
+              )
+      user.fb_userid = fb_data.id
+      user.skip_confirmation!
+      user.save
       user
-    else
-      create_from_facebook data
     end
-  end
 
-  def self.create_from_facebook(fb_data)
-    generated_password = Devise.friendly_token.first(10)
-    user = User.new( :email => fb_data.email,
-                :alias      => fb_data.username || fb_data.name,
-                :password   => generated_password,
-                :password_confirmation => generated_password,
-            )
-    user.fb_userid = fb_data.id
-    user.skip_confirmation!
-    user.save
-    user
-  end
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email         = params[:email]
-        user.alias         = params[:alias]
-        user.fb_userid     = data["id"]
-        generated_password = Devise.friendly_token.first(10)
-        user.password      = generated_password
-        user.password_confirmation = generated_password
-        user.skip_confirmation!
+    def new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email         = params[:email]
+          user.alias         = params[:alias]
+          user.fb_userid     = data["id"]
+          generated_password = Devise.friendly_token.first(10)
+          user.password      = generated_password
+          user.password_confirmation = generated_password
+          user.skip_confirmation!
+        end
       end
     end
-  end
+
+    def active
+      # Los usuarios migrados de WP tienen confirmed_at
+      # pero no necesariamente last_sign_in_at
+      where('last_sign_in_at IS NOT NULL OR confirmed_at IS NOT NULL')
+    end
+  end # End class methods
 
   # Overrides Devise method to allow non-password updates for Facebook users
   def update_with_password(params={})
@@ -84,12 +92,6 @@ class User < ActiveRecord::Base
 
   def active?
     last_sign_in_at.present? || confirmed_at.present?
-  end
-
-  def self.active
-    # Los usuarios migrados de WP tienen confirmed_ar
-    # pero no necesariamente last_sign_in_at
-    where('last_sign_in_at IS NOT NULL OR confirmed_at IS NOT NULL')
   end
 
   def has_facebook_profile?
