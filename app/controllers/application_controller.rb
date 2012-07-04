ed4f360359c38e1843fef1274a5772d9a9ebd202
Juan Schwindt <juan@schwindt.org>
2012-07-04 15:59:14 -0300

    Improve redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     render '403', :status => 403, :alert => exception.message
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    request.env['omniauth.origin'] || stored_location_for(resource_or_scope) || root_path
+  end
 end
