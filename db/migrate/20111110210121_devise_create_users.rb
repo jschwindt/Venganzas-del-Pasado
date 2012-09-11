5a839cd26508cf2bf60458b8a89b27ec301fe3c2
fcingolani <mail@fcingolani.com.ar>
2012-09-10 23:09:16 -0300

    Devise migrations updated to Devise 2.0. See https://github.com/plataformatec/devise/wiki/How-To:-Upgrade-to-Devise-2.0-migration-schema-style

--- db/migrate/20111110210121_devise_create_users.rb
+++ db/migrate/20111110210121_devise_create_users.rb
@@ -1,7 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
 
       t.string :alias, :null => false
       t.string :slug
@@ -9,10 +11,26 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.integer :karma
       t.string :fb_userid
 
-      t.recoverable
-      t.rememberable
-      t.trackable
-      t.confirmable
+    
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+    
+      ## Rememberable
+      t.datetime :remember_created_at
+    
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+    
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
 
       # t.encryptable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
