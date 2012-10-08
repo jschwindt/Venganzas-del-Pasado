f43db446671bfc5caadbd5e1220c598e401e1dda
fcingolani <mail@fcingolani.com.ar>
2012-10-08 01:22:35 -0300

    Fixed Kaminari customization

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -12,7 +12,7 @@ module Kaminari
   module Helpers
     class Tag
       def page_url_for(page)
-        @template.url_for @template.params.merge(@param_name => (page < 1 ? nil : page))
+        @template.url_for @params.merge(@param_name => (page < 1 ? nil : page))
       end
     end
   end
