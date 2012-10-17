93c921a6281c4a56ecac1bed1e93a6a207b214dd
fcingolani <mail@fcingolani.com.ar>
2012-10-17 00:32:31 -0300

    New footer design.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :smilies
+  before_filter :get_last_post
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render '404', :status => 404
@@ -13,4 +14,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     request.env['omniauth.origin'] || stored_location_for(resource_or_scope) || root_path
   end
+  
+  private
+  
+  def get_last_post
+    @last_post = Post.published.lifo.first
+  end
+  
 end
