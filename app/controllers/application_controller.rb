2dcc073ea3f6bf86061ad5f43d7f214b89d0f85b
Juan Schwindt <juan@schwindt.org>
2012-11-13 11:16:54 -0300

    Move footer cache block to fix html5/flash link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :smilies
-  before_filter :get_last_post
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render '404', :status => 404
@@ -14,11 +13,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     request.env['omniauth.origin'] || stored_location_for(resource_or_scope) || root_path
   end
-  
-  private
-  
-  def get_last_post
-    @last_post = Post.published.lifo.first
-  end
-  
+
 end
