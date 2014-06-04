0635f890cb074f8c5c5795ae98188381a22a6107
Juan Schwindt <juan@schwindt.org>
2014-06-04 12:08:13 -0300

    Fix I18n deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module VenganzasDelPasado
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.enforce_available_locales = false
     config.i18n.default_locale = :es
 
     # Configure the default encoding used in templates for Ruby 1.9.
