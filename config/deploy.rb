2fb4e854bd56b65af2e1bc72ce3be7142920a468
Juan Schwindt <juan@schwindt.org>
2016-03-09 11:53:56 -0300

    Add thinking_sphinx to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/var/www/venganzasdelpasado.com.ar'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/thinking_sphinx.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/uploads')
