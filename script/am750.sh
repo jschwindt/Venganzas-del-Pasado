3dd6dc702281359c074e49b7f85fabec0d47cf3e
Juan Schwindt <juan@schwindt.org>
2018-10-09 11:09:26 -0300

    Improve stream capturing

--- script/am750.sh
+++ script/am750.sh
@@ -1,24 +1,6 @@
 #!/bin/sh
 
+URL=`curl -s https://www.enlaradio.com.ar/listen/radio-am750_1147 | grep source | grep -Po 'src="\K.*?(?=")'`
 HOME=/var/www/venganzasdelpasado.com.ar/st
-DURATION=7200
-DATE=`/bin/date +%F -d yesterday`
 
-cd $HOME
-/bin/rm -rf lvst?.*
-
-# Radio AM 750
-/usr/bin/cvlc http://ecuamedios.org:8050/AM750_AAC --sout file/asf:lvst0.asf &
-/bin/sleep $DURATION
-kill $!
-
-/usr/bin/nice -n 15 /usr/bin/mplayer -ao pcm:file=lvst0.wav lvst0.asf
-/usr/bin/nice -n 15 /usr/bin/sox lvst0.wav -c 1 --replay-gain track lvst1.wav vol 0.4 compand 1,4 6:-80,-80,-75,-25,0,0 -6 -30 1
-
-/usr/bin/lame --quiet -v -h -B24 \
- --tl "La venganza sera terrible" \
- --tt "La venganza $DATE" \
- --tg "Other" \
- --ta "Alejandro Dolina" \
- --tc "Programa de Dolina en Radio AM750 del $DATE http://venganzasdelpasado.com.ar/" \
- lvst1.wav lavenganza_$DATE.mp3
+./with_ffmpeg.sh $URL $HOME
