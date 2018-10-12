8b9f8bc9805f7ec5840cc40d45403256f8341927
Juan Schwindt <juan@schwindt.org>
2018-10-12 10:31:00 -0300

    Fix absolute path for ffmpeg script

--- script/am750.sh
+++ script/am750.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 URL=`curl -s https://www.enlaradio.com.ar/listen/radio-am750_1147 | grep source | grep -Po 'src="\K.*?(?=")'`
-HOME=/var/www/venganzasdelpasado.com.ar/st
+DIR=/var/www/venganzasdelpasado.com.ar/st
 
-./with_ffmpeg.sh $URL $HOME
+/var/www/venganzasdelpasado.com.ar/current/script/with_ffmpeg.sh $URL $DIR
