44baf5b4f90fcfd4cd82f354c08becc4c4f0d863
Juan Schwindt <juanschw@amazon.com>
2020-03-21 22:16:27 -0300

    Add migration using schema.db from 4.2

---
+++ db/schema.rb
@@ -0,0 +1,113 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_22_010850) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.text "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["slug"], name: "index_articles_on_slug", unique: true
+  end
+
+  create_table "audios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "post_id"
+    t.string "url"
+    t.integer "bytes"
+    t.index ["post_id"], name: "index_audios_on_post_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.string "author"
+    t.string "author_email"
+    t.string "author_ip"
+    t.text "content"
+    t.string "status", default: "neutral"
+    t.datetime "created_at"
+    t.string "profile_picture_url"
+    t.datetime "updated_at"
+    t.index ["created_at"], name: "index_comments_on_created_at"
+    t.index ["post_id", "created_at"], name: "index_comments_on_post_id_and_created_at"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendly_id_slugs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 40
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", unique: true
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
+  create_table "media", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "asset"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_media_on_post_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "comments_count", default: 0
+    t.string "status"
+    t.string "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "contributor_id"
+    t.index ["contributor_id"], name: "index_posts_on_contributor_id"
+    t.index ["created_at"], name: "index_posts_on_created_at"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
+    t.index ["updated_at"], name: "index_posts_on_updated_at"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", limit: 128, default: "", null: false
+    t.string "alias", null: false
+    t.string "slug"
+    t.string "role"
+    t.integer "karma"
+    t.string "fb_userid"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "profile_picture_url"
+    t.string "unconfirmed_email"
+    t.index ["alias"], name: "index_users_on_alias", unique: true
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["fb_userid"], name: "index_users_on_fb_userid"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug"
+  end
+
+end
