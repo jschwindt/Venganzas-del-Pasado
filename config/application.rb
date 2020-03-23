3c315b04cb65a62629fc89776b0e8571ccf577ab
Juan Schwindt <juanschw@amazon.com>
2020-03-23 17:39:07 -0300

    Update locales to :es

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,11 @@ module VenganzasDelPasado
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Buenos Aires'
+
+    config.i18n.enforce_available_locales = false
+    config.i18n.default_locale = :es
+
   end
 end
