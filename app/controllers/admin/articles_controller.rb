7a9de9426a67e0dc001e904a182307598274b472
Juan Schwindt <juanschw@amazon.com>
2020-03-29 13:51:38 -0300

    Article Admin working

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,11 +1,25 @@
 module Admin
   class ArticlesController < BaseController
-    load_and_authorize_resource
+    before_action :load_collection, only: :index
+    before_action :load_resource, except: %i[index new create]
+    authorize_resource
 
     protected
 
     def verify_admin
       render '403', status: 403 unless current_user.can? :manage, Article
+    end
+
+    def load_collection
+      @articless = apply_scopes Article
+    end
+
+    def load_resource
+      @article = Article.friendly.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(%i[title content created_at])
     end
   end
 end
