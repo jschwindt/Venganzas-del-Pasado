8784f909f2426d3f58a79ea3ad93ce12117bb784
Juan Schwindt <juanschw@amazon.com>
2020-03-31 20:36:36 -0300

    Add popup and play-* links

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -1,8 +1,14 @@
 class AudiosController < ApplicationController
-  load_and_authorize_resource :post
+  before_action :load_post, only: [:show]
   load_and_authorize_resource :audio, through: :post
 
   def show
     render layout: 'popup'
+  end
+
+  protected
+
+  def load_post
+    @post = Post.friendly.find(params[:post_id])
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,45 @@ require('social-likes')
 require('./bulma')
 require('./timeago')
 require('./mde')
+
+$(document).ready(() => {
+
+    window.open_player = (url) => {
+        var nw;
+        nw = window.open(url, 'player', 'height=220,width=840,status=0,menubar=0,location=0,toolbar=0,scrollbars=0');
+        if (window.focus) {
+            nw.focus();
+        }
+        return false;
+    };
+
+    $('.open_player').click((event) => {
+        open_player(this.href);
+        return false;
+    });
+
+    $('a[href^="#play-"]').on('click', function (event) {
+        var audio, current_time, h_m_s, player, _i, _len, _ref;
+        event.preventDefault();
+        h_m_s = $(this).attr('href').replace('#play-', '').split(':');
+        current_time = 0;
+        if (h_m_s.length === 2) {
+            current_time = parseInt(h_m_s[0]) * 60 + parseInt(h_m_s[1]);
+        } else if (h_m_s.length === 3) {
+            current_time = parseInt(h_m_s[0]) * 3600 + parseInt(h_m_s[1]) * 60 + parseInt(h_m_s[2]);
+        } else {
+            return;
+        }
+        player = $(this).parents('article.post').first().find('audio')[0];
+        if (player && current_time > 0 && current_time < 7200) {
+            _ref = $('audio');
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                audio = _ref[_i];
+                audio.pause();
+            }
+            player.currentTime = current_time;
+            return player.play();
+        }
+    });
+
+})
