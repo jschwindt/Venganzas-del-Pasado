1cb8a40f4f76aa08a738ac9ce0eb84f0aa130655
Juan Schwindt <juan@schwindt.org>
2020-04-11 19:38:04 -0300

    Improve indentation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,16 @@
 class SearchController < ApplicationController
   def index
     @what = params[:what] == 'comments' ? 'comments' : 'posts'
-    q = params[:q].presence || '*'
 
-    post_q = Post.search(q, execute: false, load: @what == 'posts',
-                            page: params[:page],
-                            per_page: VenganzasDelPasado::Application.config.posts_search_per_page)
+    post_q = Post.search(params[:q],
+                  execute: false, load: @what == 'posts',
+                  page: params[:page],
+                  per_page: VenganzasDelPasado::Application.config.posts_search_per_page)
 
-    comment_q = Comment.search(q, execute: false, load: @what == 'comments',
-                                  page: params[:page],
-                                  per_page: VenganzasDelPasado::Application.config.comments_search_per_page)
+    comment_q = Comment.search(params[:q],
+                  execute: false, load: @what == 'comments',
+                  page: params[:page],
+                  per_page: VenganzasDelPasado::Application.config.comments_search_per_page)
 
     @posts_result, @comments_result = Searchkick.multi_search([post_q, comment_q])
 
