54ce0a9c98eacb2eacdf097e330b1f74baf9ce99
Juan Schwindt <juan@schwindt.org>
2020-04-11 16:46:15 -0300

    Remove print in search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,16 +4,15 @@ class SearchController < ApplicationController
     q = params[:q].presence || '*'
 
     post_q = Post.search(q, execute: false, load: @what == 'posts',
-      page: params[:page], per_page: VenganzasDelPasado::Application.config.posts_search_per_page)
-    
+                            page: params[:page],
+                            per_page: VenganzasDelPasado::Application.config.posts_search_per_page)
+
     comment_q = Comment.search(q, execute: false, load: @what == 'comments',
-      page: params[:page], per_page: VenganzasDelPasado::Application.config.comments_search_per_page)
+                                  page: params[:page],
+                                  per_page: VenganzasDelPasado::Application.config.comments_search_per_page)
 
     @posts_result, @comments_result = Searchkick.multi_search([post_q, comment_q])
 
     @results = @what == 'comments' ? @comments_result : @posts_result
-
-    ap @posts_result
-    ap @comments_result
   end
 end
