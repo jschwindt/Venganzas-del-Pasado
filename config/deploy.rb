2f3b879b77f38dc84ff6f6f586fc1bff81640c46
Juan Schwindt <juan@schwindt.org>
2020-04-18 15:59:04 -0300

    Deploy to final path

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:jschwindt/Venganzas-del-Pasado.git'
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/vdp6'
+set :deploy_to, '/var/www/venganzasdelpasado.com.ar'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
