09f9f392bbf59fd1c3ee699b77428375c2feb1f5
Juan Schwindt <juan@schwindt.org>
2020-04-18 00:43:29 -0300

    Improve Seek Play button and add #play-h:mm:ss to post url

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,26 +31,34 @@ $(document).ready(() => {
         return open_player(event.currentTarget.href);
     });
 
-    $('a[href^="#play-"]').on('click', (event) => {
-        var target, audio, current_time, h_m_s, player, _i, _len, _ref;
+    $('a[data-toggle="collapse"]').on('click', (event) => {
         event.preventDefault();
         target = event.currentTarget;
-        h_m_s = $(target).attr('href').replace('#play-', '').split(':');
-        current_time = 0;
-        if (h_m_s.length === 2) {
-            current_time = parseInt(h_m_s[0]) * 60 + parseInt(h_m_s[1]);
-        } else if (h_m_s.length === 3) {
-            current_time = parseInt(h_m_s[0]) * 3600 + parseInt(h_m_s[1]) * 60 + parseInt(h_m_s[2]);
-        } else {
-            return;
+        $(target).next('ul.archive-months').toggle();
+    });
+
+    function stop_all_players() {
+        var ref = $('audio');
+        for (var _i = 0, _len = ref.length; _i < _len; _i++) {
+            ref[_i].pause();
         }
-        player = $(target).parents('article.post').first().find('audio')[0];
-        if (player && current_time > 0 && current_time < 8000) {
-            _ref = $('audio');
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                audio = _ref[_i];
-                audio.pause();
-            }
+    }
+
+    function seconds_from_hash(hash) {
+        var h_m_s = hash.replace('#play-', '').split(':');
+        var seconds = 0;
+        if (h_m_s.length === 2) {
+            seconds = parseInt(h_m_s[0]) * 60 + parseInt(h_m_s[1]);
+        } else if (h_m_s.length === 3) {
+            seconds = parseInt(h_m_s[0]) * 3600 + parseInt(h_m_s[1]) * 60 + parseInt(h_m_s[2]);
+        }
+        return seconds < 8000 ? seconds : 0;
+    }
+
+    function seek_and_play(href, player) {
+        var current_time = seconds_from_hash(href);
+        if (player && current_time > 0) {
+            stop_all_players();
             player.currentTime = current_time;
             player.play();
             player.oncanplay = (e) => {
@@ -59,12 +67,21 @@ $(document).ready(() => {
                 }
             }
         }
-    });
+    }
 
-    $('a[data-toggle="collapse"]').on('click', (event) => {
+    $(document).on('click', 'a[href^="#play-"]', (event) => {
+        var target, player;
         event.preventDefault();
         target = event.currentTarget;
-        $(target).next('ul.archive-months').toggle();
+        player = $(target).parents('article.post').first().find('audio')[0];
+        seek_and_play($(target).attr('href'), player);
     });
 
+    if (window.location.pathname.startsWith('/posts/') && window.location.hash.startsWith('#play-')) {
+        var audios = $('audio')
+        var seconds = seconds_from_hash(window.location.hash);
+        if (audios.length > 0 && seconds > 0) {
+            $('h1.title').append('<a href="' + window.location.hash + '">' + window.location.hash.split('-')[1] + '</a>')
+        }
+    }
 })
