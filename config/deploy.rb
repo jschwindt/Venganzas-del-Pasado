6a3653fc2b6e9666858c90662d1f03e6560926ed
Juan Schwindt <juan@schwindt.org>
2020-04-19 01:22:47 -0300

    Switch to credentials by environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, '/var/www/venganzasdelpasado.com.ar'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/master.key'
+append :linked_files, 'config/database.yml', 'config/credentials/production.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
