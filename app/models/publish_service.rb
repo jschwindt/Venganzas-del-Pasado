179bff0e3095c72574104007cc0eda4afd53e463
Juan Schwindt <juan@schwindt.org>
2021-05-23 21:42:26 -0300

    New publication workflow

---
+++ app/models/publish_service.rb
@@ -0,0 +1,52 @@
+class PublishService
+  def initialize
+    @publish_files = Dir["#{VenganzasDelPasado::Application.config.x.audios_root}/publish/*.txt"]
+  end
+
+  def run
+    @publish_files.each do |publish_file|
+      File.readlines(publish_file).each do |line|
+        if line.match(%r[(/st\d*/)(lavenganza_(\d{4})-\d{2}-\d{2}.mp3)])
+          process(Regexp.last_match(0), Regexp.last_match(2), Regexp.last_match(3))
+        end
+      end
+    end
+    FileUtils.rm @publish_files
+  end
+
+  def process(source_mp3_file, mp3_file_name, year)
+    return unless File.exist?("#{VenganzasDelPasado::Application.config.x.audios_root}#{source_mp3_file}")
+
+    copy_mp3(source_mp3_file, year)
+    create_post("#{VenganzasDelPasado::Application.config.x.audios_root}/#{year}/#{mp3_file_name}")
+    sync_s3(year)
+    generate_sitemap
+  end
+
+  def copy_mp3(source_mp3_file, year)
+    source = "#{VenganzasDelPasado::Application.config.x.audios_root}#{source_mp3_file}"
+    dest = "#{VenganzasDelPasado::Application.config.x.audios_root}/#{year}/"
+    puts "Copying #{source} -> #{dest}"
+    FileUtils.cp(source, dest)
+  end
+
+  def create_post(audio_file)
+    post = Post.create_from_audio_file(audio_file)
+    audio = post.audios.first
+    file_size = File.size audio_file
+    audio.update_attributes(bytes: file_size, speech_to_text_status: :unavailable)
+    puts "#{post.inspect}\n#{audio.inspect}"
+  end
+
+  def sync_s3(year)
+    source = "#{VenganzasDelPasado::Application.config.x.audios_root}/#{year}/"
+    dest = "s3://s3.schwindt.org/dolina/#{year}/"
+    result = `aws s3 sync #{source} #{dest} --acl public-read --no-progress`
+    puts result if result.present?
+  end
+
+  def generate_sitemap
+    Rails.application.load_tasks
+    Rake::Task['sitemap:refresh'].invoke
+  end
+end
--- script/select_source.rb
+++ script/select_source.rb
@@ -1,36 +1,34 @@
 #!/usr/bin/env ruby
 
 require 'date'
-require 'net/https'
+require 'yaml'
 
-yesterday = Date.today - 1
-year = yesterday.strftime('%Y')
-mp3_file = "lavenganza_#{yesterday.strftime('%Y-%m-%d')}.mp3"
+app_config_file = File.expand_path('../config/app_config.yml', __dir__)
+app_config = YAML.load_file(app_config_file)[ENV['RAILS_ENV'] || 'development']
+
+day = Date.today - (ARGV[0] || 1).to_i
+year = day.strftime('%Y')
+mp3_file = "lavenganza_#{day.strftime('%Y-%m-%d')}.mp3"
 
 SOURCES = [
-  { url: "https://venganzasdelpasado.com.ar/st1/#{mp3_file}", min_size: 19_000_000 },
-  { url: "https://venganzasdelpasado.com.ar/st/#{mp3_file}",  min_size: 25_000_000 },
-  { url: "https://venganzasdelpasado.com.ar/st2/#{mp3_file}", min_size: 25_000_000 },
-  { url: "https://venganzasdelpasado.com.ar/st3/#{mp3_file}", min_size: 20_000_000 }
+  { file: "/st1/#{mp3_file}", min_size: 19_000_000 },
+  { file: "/st/#{mp3_file}",  min_size: 25_000_000 },
+  { file: "/st2/#{mp3_file}", min_size: 25_000_000 },
+  { file: "/st3/#{mp3_file}", min_size: 20_000_000 }
 ].freeze
 
-def get_file_length(url)
-  uri = URI(url)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  response = http.request(Net::HTTP::Head.new(uri.request_uri))
-  response ? response['content-length'].to_i : 0
-end
+dest_file = "#{app_config['audios_root']}/#{year}/#{mp3_file}"
 
-dest_length = get_file_length "https://venganzasdelpasado.com.ar/#{year}/#{mp3_file}"
-
-exit 0 if dest_length > 13_000_000
+exit 0 if File.exist?(dest_file) && File.size(dest_file) > 15_000_000
 
 SOURCES.each do |source|
-  url = source[:url]
-  source_length = get_file_length url
-  if source_length > source[:min_size]
-    exec "/var/www/venganzasdelpasado.com.ar/current/script/retrieve_and_upload.sh #{url}"
+  source_file = "#{app_config['audios_root']}#{source[:file]}"
+  if File.exist?(source_file) && File.size(source_file) > source[:min_size]
+    puts "SIRVE: #{source[:file]} (#{File.size(source_file) / (1024 * 1024)} Mb)"
+    File.open("#{app_config['audios_root']}/publish/#{Time.now.to_i}.txt", 'w') do |f|
+      f.write source[:file]
+    end
+    exit 0
   end
 end
 
