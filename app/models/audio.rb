263c3adffd4135c79257eb3641e41c30db07fd6c
Juan Schwindt <juan@schwindt.org>
2021-07-17 17:51:57 -0300

    Add ad free audio

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,5 +1,5 @@
 class Audio < ApplicationRecord
-  enum speech_to_text_status: [:unavailable, :processing, :available]
+  enum speech_to_text_status: %i[unavailable processing available]
   belongs_to :post
 
   has_many :texts, dependent: :destroy
@@ -14,5 +14,13 @@ class Audio < ApplicationRecord
     # Las contribuciones de los usuarios tienen _id de media antes de .mp3
     url.sub %r[.*//venganzasdelpasado.com.ar(/\d{4}/lavenganza_\d{4}-\d{2}-\d{2}.*\.mp3)$],
             'https://s3.amazonaws.com/s3.schwindt.org/dolina\1?torrent'
+  end
+
+  def sp_url
+    filename = url.sub %r[.*//venganzasdelpasado.com.ar/\d{4}/lavenganza_((\d{4})-\d{2}-\d{2}).*\.mp3$],
+                       '/\2/lvstsp-\1.mp3'
+    if File.exist?(VenganzasDelPasado::Application.config.x.audios_root + filename)
+      "https://venganzasdelpasado.com.ar#{filename}"
+    end
   end
 end
