ec550f18198fb7c7c073e05b47ef12fb6aa721f9
Juan Schwindt <juan@schwindt.org>
2021-08-05 13:36:24 -0300

    Use https://www.radio-browser.info/ to get AM750 streaming url

--- script/am750.sh
+++ script/am750.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-URL=`curl -s https://www.enlaradio.com.ar/listen/radio-am750_1147/ | grep source | grep -Po 'src="\K.*?(?=")'`
+URL=`curl -s http://all.api.radio-browser.info/json/stations/byuuid/2b1094f2-9a8b-4444-8568-04f4ffaaee1f | jq -r ".[].url"`
 DIR=/var/www/venganzasdelpasado.com.ar/st
 
 /var/www/venganzasdelpasado.com.ar/current/script/with_ffmpeg.sh $URL $DIR
