0e13092492837e6748d9f4d08f8d94136486fe38
Juan Schwindt <juan@schwindt.org>
2023-02-24 18:45:53 -0300

    Add MeiliSearch timeout to config

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -2,4 +2,6 @@ MeiliSearch::Rails.configuration = {
   meilisearch_url: 'http://localhost:7700',
   meilisearch_api_key: Rails.application.credentials.meilisearch[:api_key],
   pagination_backend: :kaminari,
+  timeout: 3,
+  max_retries: 2,
 }
