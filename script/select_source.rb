5253d3d943abc3ec310464666dc4c45000fae0e0
Juan Schwindt <juan@schwindt.org>
2023-04-15 19:36:46 -0300

    Add Show Start detection

--- script/select_source.rb
+++ script/select_source.rb
@@ -2,9 +2,11 @@
 
 require 'date'
 require 'yaml'
+require 'fileutils'
 
 app_config_file = File.expand_path('../config/app_config.yml', __dir__)
 app_config = YAML.load_file(app_config_file)[ENV['RAILS_ENV'] || 'development']
+find_show_start_cmd = app_config['find_show_start']
 
 day = Date.today - (ARGV[0] || 1).to_i
 year = day.strftime('%Y')
@@ -25,6 +27,13 @@ SOURCES.each do |source|
   source_file = "#{app_config['audios_root']}#{source[:file]}"
   if File.exist?(source_file) && File.size(source_file) > source[:min_size]
     puts "SIRVE: #{source[:file]} (#{File.size(source_file) / (1024 * 1024)} Mb)"
+    FileUtils.cp(source_file, "#{source_file}-bak.mp3")
+    system("ffmpeg -loglevel panic -y -i #{source_file} -t 00:10:00 -c copy #{source_file}-start.mp3")
+    show_start_time = `#{find_show_start_cmd} #{source_file}-start.mp3`.to_f
+    puts "Show start time: #{show_start_time}"
+    if show_start_time > 0
+      system("ffmpeg -loglevel panic -y -i #{source_file}-bak.mp3 -ss #{show_start_time} -c copy #{source_file}")
+    end
     File.open("#{app_config['audios_root']}/publish/#{Time.now.to_i}.txt", 'w') do |f|
       f.write source[:file]
     end
