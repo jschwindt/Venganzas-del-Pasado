5fac943f48cd085fc93eeeb453b8d553b3b6d71a
Juan Schwindt <juan@schwindt.org>
2024-08-18 11:03:14 -0300

    5 posts for the home page

--- config/initializers/venganzasdelpasado.rb
+++ config/initializers/venganzasdelpasado.rb
@@ -1,9 +1,8 @@
 module VenganzasDelPasado
-
   class Application
     config.bad_user_karma_treshold = 0
     config.good_user_karma_treshold = 500
-    config.home_posts_count = 3
+    config.home_posts_count = 5
     config.home_comments_count = 5
     config.posts_per_page = 10
     config.posts_per_page_rss = 30
@@ -11,5 +10,4 @@ module VenganzasDelPasado
     config.posts_search_per_page = 5
     config.comments_search_per_page = 10
   end
-
 end
