71ea2f2379362c6de998afb9ccba93d78ace5666
Juan Schwindt <juan@schwindt.org>
2024-11-09 20:27:50 -0300

    Upgrade gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.19.1'
+lock '~> 3.19.2'
 
 set :application, 'venganzas_del_pasado'
 set :repo_url, 'git@github.com:jschwindt/Venganzas-del-Pasado.git'
