233039b0fd5284030cbf50d399b1c8815f78b4e8
Juan Schwindt <juan@schwindt.org>
2025-10-02 09:46:34 -0300

    Add sitemap config

---
+++ config/sitemap.rb
@@ -0,0 +1,33 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://venganzasdelpasado.com.ar"
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+
+  add posts_path, :changefreq => 'daily'
+
+  Post.published.lifo.find_each do |post|
+    add post_path(post, transcription: true), :lastmod => post.updated_at, :changefreq => 'daily', :priority => 0.8
+  end
+
+  Article.find_each do |article|
+    add article_path(article), :lastmod => article.updated_at
+  end
+
+  User.find_each do |user|
+    add user_path(user), :lastmod => user.updated_at
+  end
+
+
+
+end
